545d64150d7c7ad32ed5cb8d2da610536dc9caf2
Stefano Rigat <stefano@modelresine.it>
2020-01-12 19:01:03 +0100

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+namespace DatingApp.API.Data
+{
+    using DatingApp.API.Models;
+    using Microsoft.EntityFrameworkCore;
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
