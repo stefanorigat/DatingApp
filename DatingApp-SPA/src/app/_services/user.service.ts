2c82c670c5225dccdeddba00aa0f574d58f61349
Stefano Rigat <stefano@modelresine.it>
2020-02-11 10:17:12 +0100

    End of Section 15

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -15,7 +15,7 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  getUsers(page?, itemsPerPage?, userParams?): Observable<PaginatedResults<User[]>> {
+  getUsers(page?, itemsPerPage?, userParams?, likesParams?): Observable<PaginatedResults<User[]>> {
 
     const paginatedResult: PaginatedResults<User[]> = new PaginatedResults<User[]>();
 
@@ -31,6 +31,16 @@ export class UserService {
       params = params.append('maxAge', userParams.maxAge);
       params = params.append('gender', userParams.gender);
       params = params.append('orderBy', userParams.orderBy);
+    }
+
+    if (likesParams === 'Likers')
+    {
+      params = params.append('likers', 'true');
+    }
+
+    if (likesParams === 'Likees')
+    {
+      params = params.append('likees', 'true');
     }
 
     return this.http.get<User[]>(this.baseUrl + 'users', { observe: 'response', params})
@@ -59,5 +69,9 @@ export class UserService {
 
   deletePhoto(userId: number, id: number) {
     return this.http.delete(this.baseUrl + 'users/' + userId + '/photos/' + id);
+  }
+
+  sendLike(id: number, recipientId: number) {
+    return this.http.post(this.baseUrl + 'users/' + id + '/like/' + recipientId, {});
   }
 }
--- DatingApp-SPA/src/app/lists/lists.component.ts
+++ DatingApp-SPA/src/app/lists/lists.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Pagination, PaginatedResults } from '../_models/pagination';
+import { AuthService } from '../_services/auth.service';
+import { UserService } from '../_services/user.service';
+import { ActivatedRoute } from '@angular/router';
+import { AlertifyService } from '../_services/alertify.service';
+import { User } from '../_models/user';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-lists',
@@ -6,10 +13,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lists.component.css']
 })
 export class ListsComponent implements OnInit {
+  users: User[];
+  pagination: Pagination;
+  likesParam: string;
 
-  constructor() { }
+  constructor(private authService: AuthService, private userService: UserService,
+              private activatedRoute: ActivatedRoute, private alertifyService: AlertifyService) { }
 
   ngOnInit() {
+    this.activatedRoute.data.subscribe(data => {
+      this.users = data['users'].result;
+      this.pagination = data['users'].pagination;
+    });
+
+    if (this.likesParam == null) {
+      this.likesParam = 'Likers';
+      this.loadUsers();
+    }
   }
 
+  loadUsers() {
+    this.userService.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage, null, this.likesParam)
+      .subscribe((res: PaginatedResults<User[]>) => {
+        this.users = res.result;
+        this.pagination = res.pagination;
+    }, error => {
+      this.alertifyService.error(error);
+    });
+  }
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
+  }
 }
