007894bb7f7fb105846fc087101d3fd07a09002b
Stefano Rigat <stefano@modelresine.it>
2020-02-12 18:21:21 +0100

    End of Section 16

---
+++ DatingApp-SPA/src/app/_resolvers/messages.resolver.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Resolve, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { UserService } from '../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+import { AuthService } from '../_services/auth.service';
+
+@Injectable()
+
+export class MessagesResolver implements Resolve<Message[]> {
+    pageNumber = 1;
+    pageSize = 5;
+    messageContainer = 'Unread';
+
+    constructor(private userService: UserService, private router: Router,
+                private alertify: AlertifyService, private authService: AuthService) {}
+
+    resolve(route: ActivatedRouteSnapshot): Observable<Message[]> {
+        return this.userService.getMessages(this.authService.decodedToken.nameid[0], 
+                this.pageNumber, this.pageSize, this.messageContainer).pipe(
+            catchError(error => {
+                this.alertify.error('Error retrieving data');
+                this.router.navigate(['/home']);
+                return of(null);
+            })
+        );
+    }
+}
--- DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
+++ DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from 'src/app/_models/user';
 import { UserService } from 'src/app/_services/user.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { ActivatedRoute } from '@angular/router';
 import { NgxGalleryModule } from 'ngx-gallery';
 import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+import { TabsetComponent } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-member-detail',
@@ -14,15 +15,20 @@ import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gal
 export class MemberDetailComponent implements OnInit {
 
   user: User;
-  constructor(private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
-
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
+  @ViewChild('memberTabs', {static: true}) memberTabs: TabsetComponent;
+  constructor(private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.user = data['user'];
     });
+
+    this.route.queryParams.subscribe(params => {
+      const selectedTab = params['tab'];
+      this.memberTabs.tabs[selectedTab > 0 ? selectedTab : 0].active = true;
+    })
 
     this.galleryOptions = [
       {
@@ -51,6 +57,10 @@ export class MemberDetailComponent implements OnInit {
     }
     return imgUrls;
 
+  }
+
+  selectTab(tabId: number) {
+    this.memberTabs.tabs[tabId].active = true;
   }
 
  // loadUser() {
